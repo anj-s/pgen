d806c64869cd5a12c238ba492e04228896ef89ea
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-06-07 11:40:09 -0700

    first commit of ce pgen AI

---
+++ scripts/content.js
@@ -0,0 +1,24 @@
+const article = document.querySelector('button');
+console.log("Starting app..")
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = 'Sign Up';
+
+  // Image link for pgen
+  const badge = document.createElement('a');
+  badge.classList.add('color-secondary-text', 'type--caption');
+  badge.textContent = `Get pgen password`;
+  const url = chrome.runtime.getURL('popup.html');
+  console.log(url)
+  
+  const matches = [];
+
+  for (const button of document.querySelectorAll('button')) {
+    if (button.textContent.includes(text)) {
+      matches.push(button);
+      button.insertAdjacentHTML("afterend", "<a id=" + "my link " + "href=" + url + " >create pgen password</a>")
+    }
+  }
+  console.log(matches);
+}
